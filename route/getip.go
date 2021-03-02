30801fa2d4e2891d55aff8697691697af2877cf5
MidSmer <3w.zhong.qiang@gmail.com>
2021-03-02 21:33:52 +0800

    get ip

--- route/getip.go
+++ route/getip.go
@@ -1,12 +1,13 @@
 package route
 
 import (
+	"errors"
 	"net"
 	"net/http"
 )
 
 func GetIP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
 	IPAddress := r.Header.Get("X-Real-Ip")
 	if IPAddress == "" {
@@ -16,17 +17,32 @@ func GetIP(w http.ResponseWriter, r *http.Request) {
 		IPAddress = r.RemoteAddr
 	}
 
+	getIP4 := func(addr string) (ip4 string, err error) {
+		userIP := net.ParseIP(addr)
+		if userIP.To4() == nil {
+			return "", errors.New("not ip4")
+		}
+
+		return userIP.To4().String(), nil
+	}
+
 	ip, _, err := net.SplitHostPort(IPAddress)
 	if err != nil {
-		w.Write([]byte("error IP" + IPAddress))
+		ip4, err := getIP4(IPAddress)
+		if err != nil {
+			w.Write([]byte(""))
+			return
+		}
+
+		w.Write([]byte(ip4))
 		return
 	}
 
-	userIP := net.ParseIP(ip)
-	if userIP.To4() == nil {
-		w.Write([]byte("not IP4" + ip))
+	ip4, err := getIP4(ip)
+	if err != nil {
+		w.Write([]byte(""))
 		return
 	}
 
-	w.Write([]byte(userIP.To4().String()))
+	w.Write([]byte(ip4))
 }
--- route/home.go
+++ route/home.go
@@ -3,6 +3,6 @@ package route
 import "net/http"
 
 func ServeHome(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Write([]byte("Running"))
 }
